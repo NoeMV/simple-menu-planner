dde91d3ab92a2adb8cbba476bc31a404bdf77d24
NoeMV <kilonoe@gmail.com>
2023-03-12 18:15:52 -0600

    Updated home view
    Home view was changed with the new UI conventions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,29 +4,29 @@
 
 <template>
     <div class="mx-auto max-w-7xl px-10 h-full flex justify-center items-center">
-        <div class="py-5 sm:py-14 px-10 flex flex-col justify-center items-center space-y-8 bg-white bg-opacity-20 backdrop-blur-md drop-shadow-sm rounded-lg shadow-lg">
-            <h1 class="font-signika-negative font-semibold text-2xl md:text-4xl text-center text-sky-600">Simple menu planner</h1>
-            <p class="font-signika-negative font-medium text-base md:text-xl text-justify text-slate-700">
+        <div class="py-5 sm:py-14 px-10 flex flex-col justify-center items-center space-y-4 sm:space-y-8 bg-slate-100 rounded-xl shadow-xl">
+            <h1 class="font-signika-negative font-semibold text-2xl md:text-4xl text-center text-slate-600">Simple menu planner</h1>
+            <p class="font-signika-negative font-medium text-base md:text-xl text-justify text-slate-600">
                 Simple menu planner es una aplicación web para planificar comidas semanalmente, llevando un registro de qué se ha cocinado 
                 recientemente y proporciona sugerencias de comidas basado en distintas variables como las preferencias del usuario, la última
                 vez que un platillo ha sido cocinado, su complejidad, los ingedientes que lleva, etc.
             </p>
             <div class="flex justify-start w-full">
-                <ul class="font-signika-negative font-medium text-base md:text-xl text-slate-700 space-y-3">
+                <ul class="font-signika-negative font-medium text-base md:text-xl text-slate-600 space-y-1 sm:space-y-3">
                     <li class="flex items-center ml-10 relative">
-                        <div class="absolute rounded-full bg-rose-500 h-3 w-3 -left-6"></div>
+                        <div class="absolute rounded-full bg-sky-500 h-3 w-3 -left-6"></div>
                         <p>Organiza tus comidas en un solo lugar</p>
                     </li>
                     <li class="flex items-center ml-10 relative">
-                        <div class="absolute rounded-full bg-rose-500 h-3 w-3 -left-6"></div>
+                        <div class="absolute rounded-full bg-sky-500 h-3 w-3 -left-6"></div>
                         <p>Planea lo que cocinarás en la semana</p>
                     </li>
                     <li class="flex items-center ml-10 relative">
-                        <div class="absolute rounded-full bg-rose-500 h-3 w-3 -left-6"></div>
+                        <div class="absolute rounded-full bg-sky-500 h-3 w-3 -left-6"></div>
                         <p>Recibe sugerencias personalizadas para cocinar</p>
                     </li>
                     <li class="flex items-center ml-10 relative">
-                        <div class="absolute rounded-full bg-rose-500 h-3 w-3 -left-6"></div>
+                        <div class="absolute rounded-full bg-sky-500 h-3 w-3 -left-6"></div>
                         <p>Invita a otros a formar parte de tu menu</p>
                     </li>
                 </ul>
